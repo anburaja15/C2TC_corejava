d521778a437958e3d7e91dd3a14dfcedd5e17e5f
simbh <simbh@LAPTOP-CDFMAFM9>
2024-07-29 20:51:54 +0800

    constructor

---
+++ constuctor/src/defalutconstuctor/class1.java
@@ -0,0 +1,21 @@
+package defalutconstuctor;
+
+public class class1 {
+ //default Constructor
+	class1()
+	{
+		System.out.println("default constructer");		
+	}
+	//parameterized constructor
+	class1(int a)
+	{
+     System.out.println("A"+a);
+	}
+	class1(int b,String c){
+		System.out.println("b"+b+"c"+c);	
+	}
+	class1(String d){
+		System.out.println("d"+d);
+	}
+
+}
---
+++ constuctor/src/defalutconstuctor/class2.java
@@ -0,0 +1,14 @@
+package defalutconstuctor;
+
+public class class2 {
+
+	public static void main(String[] args) {
+		class1 c1=new class1();
+		class1 c2=new class1(5);
+		class1 c3=new class1(5+"anbu");
+		class1 c4=new class1("welcome to project");
+		
+
+	}
+
+}
